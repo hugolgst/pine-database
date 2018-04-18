f8a2d12698d715e44d5ebb266888f1803397f906
iambluedev <iambluedev@gmx.fr>
2018-04-18 11:37:06 +0200

    add comments

--- src/main/java/xyz/anana/pine/core/connector/MySQLConnector.java
+++ src/main/java/xyz/anana/pine/core/connector/MySQLConnector.java
@@ -4,11 +4,21 @@ import fr.iambluedev.pine.api.connector.IConnector;
 
 public class MySQLConnector implements IConnector {
 
+	/**
+	 * Get the name of the connector
+	 * 
+	 * @return connector's name
+	 */
 	@Override
 	public String getName() {
 		return "mysql";
 	}
 
+	/**
+	 * Get the driver path of the connector
+	 * 
+	 * @return connector's driver path
+	 */
 	@Override
 	public String getDriverPath() {
 		return  "com.mysql.jdbc.Driver";
