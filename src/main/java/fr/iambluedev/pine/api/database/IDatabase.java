14870c8290cafebe95a0a97c45eae6d991412b33
iambluedev <iambluedev@gmx.fr>
2018-04-18 11:38:06 +0200

    add comments

--- src/main/java/fr/iambluedev/pine/api/database/IDatabase.java
+++ src/main/java/fr/iambluedev/pine/api/database/IDatabase.java
@@ -8,15 +8,40 @@ import fr.iambluedev.pine.api.table.ITable;
 
 public interface IDatabase {
 
+	/**
+     * Retrieves a table into this database with his name.
+     *
+     * @param name The table name.
+     * @return The table object.
+     */
 	ITable getTable(String name);
 	
+	/**
+	 * Drop Table
+	 * 
+	 * @param name
+	 */
 	boolean dropTable(String name);
 	
+	/**
+	 * Truncate Table
+	 * 
+	 * @param name
+	 */
 	boolean truncateTable(String name);
 	
+	 /**
+     * Etablish a connection to the MySQL Database.
+     */
 	boolean connect();
 	
+	/**
+     * Close the etablished connection.
+     */
 	boolean close();
 	
+	 /**
+     * Get Database Status
+     */
 	boolean status();
 }
