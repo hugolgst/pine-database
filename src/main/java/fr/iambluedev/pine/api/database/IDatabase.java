be63cf813d958d7601784fd5de151ff5c8fa5aec
iambluedev <iambluedev@gmx.fr>
2018-04-18 11:34:26 +0200

    Add comments

--- src/main/java/fr/iambluedev/pine/api/database/IDatabase.java
+++ src/main/java/fr/iambluedev/pine/api/database/IDatabase.java
@@ -2,6 +2,10 @@ package fr.iambluedev.pine.api.database;
 
 import fr.iambluedev.pine.api.table.ITable;
 
+/**
+ * @author <a href="https://twitter.com/iambluedev">iambluedev</a>
+ */
+
 public interface IDatabase {
 
 	ITable getTable(String name);
